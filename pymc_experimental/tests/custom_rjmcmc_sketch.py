118a4dc29c3db8928de3181b1bb67c997cfc23b8
TeaWolf <TeaWolf@users.noreply.github.com>
2022-03-16 10:10:31 +0100

    refactor and simplify example script

--- pymc_experimental/tests/custom_rjmcmc_sketch.py
+++ pymc_experimental/tests/custom_rjmcmc_sketch.py
@@ -29,8 +29,9 @@ def get_analytical_solution_for_matrix(deltas, ks, initial_values):
 
     def f(t):
         return expm(M * t) @ initial_values
-    
+
     return f
+
 
 def get_vectorized_solution_for_matrix(deltas, ks, initial_values):
     f = get_analytical_solution_for_matrix(deltas, ks, initial_values)
@@ -39,7 +40,9 @@ def get_vectorized_solution_for_matrix(deltas, ks, initial_values):
         return lambda t: f(t)[i]
 
     # Just splitting this into a dictionary for each species cause I'm confused
-    return {i:np.vectorize(function_for_substance(i)) for i in range(len(initial_values))}
+    return {
+        i: np.vectorize(function_for_substance(i)) for i in range(len(initial_values))
+    }
 
 
 class LogLike(tt.Op):
@@ -83,6 +86,7 @@ class LogLike(tt.Op):
         (theta,) = inputs
         return [g[0] * self.logpgrad(theta)]
 
+
 class LogLikeGrad(tt.Op):
     """
     TODO test this somehow
@@ -116,215 +120,196 @@ class LogLikeGrad(tt.Op):
     def perform(self, node, inputs, outputs):
         (theta,) = inputs
 
-        outputs[0][0] = scipy.optimize.approx_fprime(theta, self.likelihood, epsilon= np.sqrt(np.finfo(float).eps))
+        outputs[0][0] = scipy.optimize.approx_fprime(
+            theta, self.likelihood, epsilon=np.sqrt(np.finfo(float).eps)
+        )
 
 
+class UniformJump(Jump):
+    """Placeholder class for jumps"""
 
-# Generate Data from the true model
-timepoints = np.linspace(0, 5, 1000)
-true_deltas = np.array([[0, 1],[0, 0]])
-true_ks     = np.array([[0, 2],[0, 0]])
-initial_values = np.array([5, 0])
-true_model = get_vectorized_solution_for_matrix(true_deltas,true_ks, initial_values)
-true_data = [true_model[i](timepoints) for i in range(2)]
+    # TODO figure out how to generate and gather stats properly
+    # generates_stats = True
+    # stats_dtypes = [
+    #     {
+    #         "accept": np.float64
+    #         ,"accepted": bool
+    #         ,"diverged": bool
+    #         , "tune": bool
+    #     }
+    # ]
 
-# Define loglikelihood 
-def my_loglike(vec):
-    # we'll just hardcode this cause I'm tired
+    @classmethod
+    def get_deltas_to_flip(self, src, dest):
+        return {d for i, d in enumerate(delta_parameters) if src[i] != dest[i]}
 
-    if (vec[0] != 0 and vec[0] != 1) and (vec[1] != 0 and vec[1] != 1):
-        raise(ValueError('Delta parameters can only be one or zero'))
+    def __new__(self, k_parameters, *args, **kwargs):
+        return super().__new__(self, *args, **kwargs)
 
-    deltas = np.array([[0, vec[0]],[vec[1], 0]])
-    ks = np.array([[0, vec[2]],[vec[3], 0]])
-    new_model = get_vectorized_solution_for_matrix(deltas, ks, initial_values)
-    simulated_data = [new_model[i](timepoints) for i in range(2)]
-    sum = 0
-    for i in range(2):
-        sum += np.sum(np.log(scipy.stats.norm.pdf(simulated_data[i] - true_data[i])))
+    def __init__(self, k_parameters, *args, **kwargs):
+        """
+        Creates a very simple Jump that randomely selects new k's from a uniform distribution
+        """
 
-    if not np.isfinite(sum):
-        raise(ValueError('loglikelihood diverged'))
+        self.uniform_scaling = 10
+        self.k_parameters = k_parameters
 
-    return sum
+        super().__init__(*args, **kwargs)
 
-# Generate the likelihood
-ndraws = 200
-nburn = 50
-pymc_model = pm.Model()
-theano_loglike = LogLike(lambda x: my_loglike(x))
-with pymc_model:
-    # Generate priors based on the Supermodel priors
-    var_assoc = []
-    delta_parameters = []
-    k_parameters = []
-    for i in range(2):
-        for j in range(2):
-            if i!=j:
-                delta = pm.Bernoulli('delta_{}_{}'.format(i, j), 0.5)
-                k = pm.Uniform('k_{}_{}'.format(i, j), lower=0, upper=10)
-                var_assoc.append((delta, k))
-                delta_parameters.append(delta)
-                k_parameters.append(k)
-    
-    pymc_parameters = [d for d, _ in var_assoc] + [k for _, k in var_assoc]
+    def get_subspace_updater(self):
+        # Precompute setting ks
+        ks_to_randomize = [
+            (str(k.tag.value_var), self.get_forward_transform(k))
+            for i, k in enumerate(self.k_parameters)
+            if self.dest[i] == 1 and self.src[i] == 0
+        ]
 
-    theta = tt.as_tensor_variable(pymc_parameters)
+        def set_new_ks(point):
+            for k, transform in ks_to_randomize:
+                point[k] = transform(np.random.random() * self.uniform_scaling)
 
-    pm.Potential("loglikelihood", theano_loglike(theta))
+        return set_new_ks
 
-    
+    def get_deltas_updater(self):
+        # precompute delta update
+        targets = [
+            str(d)
+            for i, d in enumerate(self.delta_parameters)
+            if self.src[i] != self.dest[i]
+        ]
 
-    class Jump(pm.step_methods.arraystep.BlockedStep):
-        """ Placeholder class for jumps"""
+        def f(point):
+            for delta in targets:
+                point[delta] = 1 - point[delta]
 
-        # TODO figure out how to generate and gather stats properly
-        # generates_stats = True
-        # stats_dtypes = [
-        #     {
-        #         "accept": np.float64
-        #         ,"accepted": bool
-        #         ,"diverged": bool
-        #         , "tune": bool
-        #     }
-        # ]
+        return f
 
-        @classmethod
-        def get_vars_from_src_dest(self, src, dest):
-            return list({k for i, k in enumerate(k_parameters) if src[i] != dest[i]}.union(self.get_deltas_to_flip(src, dest)))
+    def get_log_acceptance_fraction_calculator(self):
+        dimension_mismatch = np.sum(self.dest) - np.sum(self.src)
+        self.q_term = +dimension_mismatch * np.log(self.uniform_scaling)
 
-        @classmethod
-        def get_deltas_to_flip(self, src, dest):
-            return {d for i, d in enumerate(delta_parameters) if src[i] != dest[i]}
+        def f(new_point, point):
+            return (
+                self.logp(new_point) - self.logp(point) + self.diff_logj + self.q_term
+            )
 
-        def __new__(cls, src, dest, *args, **kwargs):
-            
-            # These have to be appended here because BlockedStep does some stuff with it in __new__
-            kwargs['vars'] = cls.get_vars_from_src_dest(src, dest)
-
-            return super().__new__(cls, *args, **kwargs)
-
-        def __init__(self, src, dest, p_src_dest, p_dest_src, *args, **kwargs):
-            """
-            Creates a very simple Jump
-            between src and dest configurations with p_src_dest, p_dest_src being the probabilities that this was selected to begin with
-            """
-            self.src = src
-            self.dest = dest
-
-            # uniform scaling (sigma * u) to generate the random var
-            self.uniform_scaling = 10
-
-            # Precompute the prior bias in having selected this move type
-            self.p_src_dest = p_src_dest
-            self.p_dest_src = p_dest_src
-            self.diff_logj = np.log(self.p_dest_src) - np.log(self.p_src_dest)
-
-            # Precompute the q term due to variable u generation
-            dimension_mismatch = np.sum(dest) - np.sum(src)
-            self.q_term = + dimension_mismatch * np.log(self.uniform_scaling)
+        return f
 
 
-            self.tune = False
-            self.model = pm.modelcontext(None)
+if __name__ == "__main__":
+    # Generate Data from the true model
+    timepoints = np.linspace(0, 5, 1000)
+    true_deltas = np.array([[0, 1], [0, 0]])
+    true_ks = np.array([[0, 2], [0, 0]])
+    initial_values = np.array([5, 0])
+    true_model = get_vectorized_solution_for_matrix(
+        true_deltas, true_ks, initial_values
+    )
+    true_data = [true_model[i](timepoints) for i in range(2)]
 
-            # Generate the dlogp function
-            self.rvs = self.get_vars_from_src_dest(src, dest)
-            self.vars = pm.inputvars([self.model.rvs_to_values.get(var, var) for var in self.rvs])
-            self.logp = self.model.compile_logp()
+    # Define loglikelihood
+    def my_loglike(vec):
+        # we'll just hardcode this cause I'm tired
 
-            # Precompute delta flips
-            deltas_to_flip = [str(x) for x in self.get_deltas_to_flip(src, dest)]
-            def flip_deltas(point):
-                for d in deltas_to_flip:
-                    point[d] = 1 - point[d]
-            self.flip_deltas = flip_deltas
+        if (vec[0] != 0 and vec[0] != 1) and (vec[1] != 0 and vec[1] != 1):
+            raise (ValueError("Delta parameters can only be one or zero"))
 
-            def get_forward_transform(rv):
-                """Returns a function which performs the forward value_var transform on a scalar"""
-                def f(x):
-                    return rv.tag.value_var.tag.transform.forward(x, *rv.owner.inputs).eval()
-                return f
+        deltas = np.array([[0, vec[0]], [vec[1], 0]])
+        ks = np.array([[0, vec[2]], [vec[3], 0]])
+        new_model = get_vectorized_solution_for_matrix(deltas, ks, initial_values)
+        simulated_data = [new_model[i](timepoints) for i in range(2)]
+        sum = 0
+        for i in range(2):
+            sum += np.sum(
+                np.log(scipy.stats.norm.pdf(simulated_data[i] - true_data[i]))
+            )
 
-            # Precompute setting ks
-            ks_to_randomize = [(str(k.tag.value_var), get_forward_transform(k)) for i, k in enumerate(k_parameters) if self.dest[i] == 1 and self.src[i] == 0]
-            def set_new_ks(point):
-                for k, transform in ks_to_randomize:
-                    point[k] = transform(np.random.random())
-            self.set_new_ks = set_new_ks
+        if not np.isfinite(sum):
+            raise (ValueError("loglikelihood diverged"))
 
-        def step(self, point):
-            """
-            point is a dictionary of {str(value_var): array(x)}
-            the transformation on the varlue variables is reversed for some of my computations
-            and then applied again
-            """
-            new_point = {x:y for x, y in point.items()}
+        return sum
 
-            # stats = {}
+    # Generate the likelihood
+    ndraws = 10
+    nburn = 15
+    pymc_model = pm.Model()
+    theano_loglike = LogLike(lambda x: my_loglike(x))
+    with pymc_model:
+        # Generate priors based on the Supermodel priors
+        var_assoc = []
+        delta_parameters = []
+        k_parameters = []
+        for i in range(2):
+            for j in range(2):
+                if i != j:
+                    delta = pm.Bernoulli("delta_{}_{}".format(i, j), 0.5)
+                    k = pm.Uniform("k_{}_{}".format(i, j), lower=0, upper=10)
+                    var_assoc.append((delta, k))
+                    delta_parameters.append(delta)
+                    k_parameters.append(k)
 
-            # discrete parameter set
-            self.flip_deltas(new_point)
+        pymc_parameters = [d for d, _ in var_assoc] + [k for _, k in var_assoc]
 
-            # Continous parameter set 
-            self.set_new_ks(new_point)
+        theta = tt.as_tensor_variable(pymc_parameters)
 
-            # The general RJMCMC kernel will have to provide this object with it
-            log_acceptance_fraction = self.logp(new_point) - self.logp(point) + self.diff_logj + self.q_term
+        pm.Potential("loglikelihood", theano_loglike(theta))
 
-            if not np.isfinite(log_acceptance_fraction):
-                raise(ValueError())
+        # Delta configurations and subspace sets
+        configurations = [(0, 1), (1, 0), (1, 1)]
+        configurations_to_subspaces = {
+            (0, 1): [pymc_model.named_vars["k_0_1"]],
+            (1, 0): [pymc_model.named_vars["k_1_0"]],
+            (1, 1): [pymc_model.named_vars["k_0_1"], pymc_model.named_vars["k_1_0"]],
+        }
 
-            # Check for acceptance
-            # stats['diverged'] = not np.isfinite(log_acceptance_fraction)
-            # stats['accept'] = log_acceptance_fraction
+        # Inter subspace jump steppers
+        jump_probas = collections.defaultdict(dict)
+        # we filter out self maps and maps to/from the zero dynamics model
+        for c1, c2 in filter(
+            lambda x: x[0] != x[1], itertools.product(configurations, repeat=2)
+        ):
+            # we'll have everything be equiprobable but no jumping towards zero dynamics
+            jump_probas[c1][c2] = 1 if c2 != tuple([0] * len(delta_parameters)) else 0
 
-            # If np.isfinite fails then we just consider we're out of bounds (diverged)
-            if np.isfinite(log_acceptance_fraction) and np.log(np.random.random()) < min(0, log_acceptance_fraction):
-                # stats['accepted'] = True
-                # return new_point, [stats]
-                return new_point
-            else:
-                # stats['accepted'] = False
-                # return point, [stats]
-                return point
+        # renormalize the jump probabilities
+        for c1 in jump_probas:
+            sum = np.sum(list(jump_probas[c1].values()))
+            jump_probas[c1] = {c2: val / sum for c2, val in jump_probas[c1].items()}
 
-    # Delta configurations and subspace sets
-    configurations = functools.reduce(lambda x,y: x.union(y), (set(itertools.permutations(x)) for x in itertools.combinations_with_replacement([0, 1], len(delta_parameters))))
-    configurations_to_subspaces = {config:{k_parameters[i] for i,d in enumerate(config) if d == 1} for config in configurations}
-    # filter out the null subspace if present
-    configurations_to_subspaces = {x:y for x,y in configurations_to_subspaces.items() if len(y) > 0}
+        # Remap jump probabilities into steppers
+        jumps = {
+            c1: {
+                c2: UniformJump(
+                    k_parameters,
+                    delta_parameters,
+                    configurations_to_subspaces,
+                    c1,
+                    c2,
+                    jump_probas[c1][c2],
+                    jump_probas[c2][c1],
+                )
+                for c2 in jump_probas[c1]
+            }
+            for c1 in jump_probas
+        }
 
-    # Inter subspace jump steppers
-    jump_probas = collections.defaultdict(dict)
-    # we filter out self maps and maps to/from the zero dynamics model
-    for c1, c2 in filter(lambda x: x[0] != x[1], itertools.product(configurations, repeat = 2)):
-         # we'll have everything be equiprobable but no jumping towards zero dynamics
-        jump_probas[c1][c2] = 1 if c2 != tuple([0] * len(delta_parameters)) else 0
+        step = RJMCMC(delta_parameters, configurations_to_subspaces, jumps, jump_probas)
 
-    # renormalize the jump probabilities
-    for c1 in jump_probas:
-        sum = np.sum(list(jump_probas[c1].values()))
-        jump_probas[c1] = {c2:val/sum for c2, val in jump_probas[c1].items()}
+        trace = pm.sample(
+            ndraws, tune=nburn, step=step, discard_tuned_samples=True, cores=1
+        )
 
-    # Remap jump probabilities into steppers
-    jumps = {c1:{c2: Jump(c1, c2, jump_probas[c1][c2], jump_probas[c2][c1]) for c2 in jump_probas[c1] } for c1 in jump_probas}
+    # # Just dump all the data inzto a pickle
+    # with open('pymc_trace.pkl', 'wb') as buff:
+    #     pickle.dump({'model':pymc_model, 'trace':trace}, buff)
 
-    step = RJMCMC(delta_parameters, configurations_to_subspaces, jumps, jump_probas)
+    # with open('pymc_trace.pkl', 'rb') as buff:
+    #     data = pickle.load(buff)
+    #     trace = data['trace']
 
-    trace = pm.sample(ndraws, tune = nburn, step = step, discard_tuned_samples = True, cores=1)
-
-# # Just dump all the data inzto a pickle
-# with open('pymc_trace.pkl', 'wb') as buff:
-#     pickle.dump({'model':pymc_model, 'trace':trace}, buff)
-
-# with open('pymc_trace.pkl', 'rb') as buff:
-#     data = pickle.load(buff)
-#     trace = data['trace']
-
-# figure_directory = 'figures/simple_reaction_reversibility/'
-# with pymc_model:
-#     az.plot_trace(trace.posterior, var_names = ['delta_0_1', 'delta_1_0'])
-#     full_screen_save(figure_directory, 'delta_trace.png')
-#     az.plot_trace(trace.posterior, var_names = ['k_0_1', 'k_1_0'])
-#     full_screen_save(figure_directory, 'k_trace.png')
+    # figure_directory = 'figures/simple_reaction_reversibility/'
+    # with pymc_model:
+    #     az.plot_trace(trace.posterior, var_names = ['delta_0_1', 'delta_1_0'])
+    #     full_screen_save(figure_directory, 'delta_trace.png')
+    #     az.plot_trace(trace.posterior, var_names = ['k_0_1', 'k_1_0'])
+    #     full_screen_save(figure_directory, 'k_trace.png')
