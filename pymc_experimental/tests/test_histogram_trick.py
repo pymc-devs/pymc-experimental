b87ead2cba8e72cf4eadb1d5f882ce6a230e5861
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2022-04-12 17:04:20 +0000

    add quantile histogram helper function

---
+++ pymc_experimental/tests/test_histogram_trick.py
@@ -0,0 +1,16 @@
+from termios import N_MOUSE
+import pymc_experimental as pmx
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize("use_dask", [False])
+def test_histogram_init(use_dask):
+    data = np.random.randn(10000)
+    
+    histogram = pmx.tricks.quantile_histogram(data, n_quantiles=100)
+    assert isinstance(histogram, dict)
+    assert histogram["mid"].shape == (99, )
+    assert histogram["low"].shape == (99, )
+    assert histogram["upper"].shape == (99, )
+    assert histogram["count"].shape == (99, )
---
+++ pymc_experimental/tricks/histogram.py
@@ -0,0 +1,22 @@
+import numpy as np
+import functools
+from numpy.typing import ArrayLike
+from typing import Dict
+
+
+__all__ = ["quantile_histogram"]
+
+
+@functools.singledispatch
+def quantile_histogram(data: ArrayLike, n_quantiles=1000) -> Dict[str, ArrayLike]:
+    quantiles = np.quantile(data, np.linspace(0, 1, n_quantiles))
+    count, _ = np.histogram(data, quantiles)
+    low = quantiles[:-1]
+    upper = quantiles[1:]
+    result = dict(
+        low=low,
+        upper=upper,
+        mid=(low + upper) / 2,
+        count=count,
+    )
+    return result
