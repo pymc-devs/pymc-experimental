7cd573535bed0c43f9e56ccef2be4f788a6efd91
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2022-04-12 18:04:51 +0000

    add thistogram for dask

--- pymc_experimental/tests/test_histogram_trick.py
+++ pymc_experimental/tests/test_histogram_trick.py
@@ -4,13 +4,15 @@ import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize("use_dask", [False])
+@pytest.mark.parametrize("use_dask", [True, False])
 def test_histogram_init(use_dask):
     data = np.random.randn(10000)
-    
+    if use_dask:
+        dask_df = pytest.importorskip("dask.dataframe")
+        data = dask_df.from_array(data)
     histogram = pmx.utils.quantile_histogram(data, n_quantiles=100)
     assert isinstance(histogram, dict)
-    assert histogram["mid"].shape == (99, )
-    assert histogram["low"].shape == (99, )
-    assert histogram["upper"].shape == (99, )
-    assert histogram["count"].shape == (99, )
+    assert histogram["mid"].shape == (99,)
+    assert histogram["low"].shape == (99,)
+    assert histogram["upper"].shape == (99,)
+    assert histogram["count"].shape == (99,)
--- pymc_experimental/utils/histogram_trick.py
+++ pymc_experimental/utils/histogram_trick.py
@@ -1,7 +1,13 @@
+import typing
 import numpy as np
 import functools
 from numpy.typing import ArrayLike
 from typing import Dict
+
+try:
+    import dask.dataframe
+except ImportError:
+    dask = None
 
 
 __all__ = ["quantile_histogram"]
@@ -9,6 +15,11 @@ __all__ = ["quantile_histogram"]
 
 @functools.singledispatch
 def quantile_histogram(data: ArrayLike, n_quantiles=1000) -> Dict[str, ArrayLike]:
+    raise NotImplementedError(f"Not implemented for {type(data)}")
+
+
+@quantile_histogram.register(np.ndarray)
+def _(data: ArrayLike, n_quantiles=1000) -> Dict[str, ArrayLike]:
     quantiles = np.quantile(data, np.linspace(0, 1, n_quantiles))
     count, _ = np.histogram(data, quantiles)
     low = quantiles[:-1]
@@ -21,3 +32,19 @@ def quantile_histogram(data: ArrayLike, n_quantiles=1000) -> Dict[str, ArrayLike
     )
     return result
 
+
+if dask is not None:
+
+    @quantile_histogram.register(dask.dataframe.Series)
+    def _(data: dask.dataframe.Series, n_quantiles=1000) -> Dict[str, ArrayLike]:
+        quantiles = data.quantile(np.linspace(0, 1, n_quantiles)).persist()
+        count, _ = dask.array.histogram(data, quantiles.compute())
+        low = quantiles.to_dask_array(lengths=True)[:-1]
+        upper = quantiles.to_dask_array(lengths=True)[1:]
+        result = dict(
+            low=low,
+            upper=upper,
+            mid=(low + upper) / 2,
+            count=count,
+        )
+        return result
