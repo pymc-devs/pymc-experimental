8323f189020a1ff826fdc265a2a26dc6de2410b5
Bill Engels <w.j.engels@gmail.com>
2022-08-07 11:22:10 -0700

    fix init

--- pymc_experimental/gp/__init__.py
+++ pymc_experimental/gp/__init__.py
@@ -2,8 +2,4 @@ from pymc_experimental.gp.latent_approx import (
     KarhunenLoeveExpansion,
     ProjectedProcess,
 )
-from pymc_experimental.gp.hsgp import (
-    HSGP,
-    ExpQuad,
-    Matern52,
-)
+from pymc_experimental.gp.hsgp import HSGP
