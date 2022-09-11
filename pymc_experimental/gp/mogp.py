755fe285b43fa29e442488c53dc83b56e01cd838
danhphan <danh.phan.mq@gmail.com>
2022-09-11 15:53:44 +1000

    add build_XY() utility

--- pymc_experimental/gp/mogp.py
+++ pymc_experimental/gp/mogp.py
@@ -1,31 +1,18 @@
 import numpy as np
 import pymc as pm
+from pymc.gp.cov import Covariance
+from pymc.gp.gp import Marginal
+from pymc.gp.util import stabilize
 
-from pymc.gp.cov import Constant, Covariance
-from pymc.gp.mean import Zero
-from pymc.gp.util import (
-    JITTER_DEFAULT,
-    cholesky,
-    conditioned_vars,
-    replace_with_values,
-    solve_lower,
-    solve_upper,
-    stabilize,
-)
-
-import aesara.tensor as at
-from pymc.gp.gp import Base, Latent, Marginal
 
 class MultiOutputMarginal(Marginal):
-
     def __init__(self, means, kernels, input_dim, active_dims, num_outputs, W=None, B=None):
         self.means = means
         self.kernels = kernels
         self.cov_func = self._get_lcm(input_dim, active_dims, num_outputs, kernels, W, B)
-        super().__init__(cov_func = self.cov_func)
+        super().__init__(cov_func=self.cov_func)
 
-
-    def _get_icm(self, input_dim, kernel, W=None, kappa=None, B=None, active_dims=None, name='ICM'):
+    def _get_icm(self, input_dim, kernel, W=None, kappa=None, B=None, active_dims=None, name="ICM"):
         """
         Builds a kernel for an Intrinsic Coregionalization Model (ICM)
         :input_dim: Input dimensionality (include the dimension of indices)
@@ -36,24 +23,29 @@ class MultiOutputMarginal(Marginal):
         :name: The name of Intrinsic Coregionalization Model
         """
 
-        coreg = pm.gp.cov.Coregion(input_dim=input_dim, W=W, kappa=kappa, B=B, active_dims=active_dims)
-        return coreg * kernel      
-        
+        coreg = pm.gp.cov.Coregion(
+            input_dim=input_dim, W=W, kappa=kappa, B=B, active_dims=active_dims
+        )
+        return coreg * kernel
 
-    def _get_lcm(self, input_dim, active_dims, num_outputs, kernels, W=None, B=None, name='ICM'):
+    def _get_lcm(self, input_dim, active_dims, num_outputs, kernels, W=None, B=None, name="ICM"):
         if B is None:
             kappa = pm.Gamma(f"{name}_kappa", alpha=5, beta=1, shape=num_outputs)
             if W is None:
-                W = pm.Normal(f"{name}_W", mu=0, sigma=5, shape=(num_outputs, 1), 
-                                                            initval=np.random.randn(num_outputs, 1))
+                W = pm.Normal(
+                    f"{name}_W",
+                    mu=0,
+                    sigma=5,
+                    shape=(num_outputs, 1),
+                    initval=np.random.randn(num_outputs, 1),
+                )
         else:
             kappa = None
             W = None
-        
+
         cov_func = 0
-        for idx, kernel in enumerate(kernels):            
-            print(B)        
-            icm = self._get_icm(input_dim, kernel, W, kappa, B, active_dims, f'{name}_{idx}')
+        for idx, kernel in enumerate(kernels):
+            icm = self._get_icm(input_dim, kernel, W, kappa, B, active_dims, f"{name}_{idx}")
             cov_func += icm
         return cov_func
 
