f3a13f5d325420249401f028b6b7edd9ee2a6064
Kunal Ghosh <kunal.ghosh@aalto.fi>
2022-09-27 20:35:26 +0300

    Added import checks in the test file as well

---
+++ pymc_experimental/tests/test_pivoted_cholesky.py
@@ -0,0 +1,23 @@
+try:
+    import torch
+    import gpytorch
+except ImportError as e:
+    raise ImportError(
+        "Please install Pytorch and GPyTorch to use this pivoted Cholesky implementation."
+    )
+import numpy as np
+import pytest
+import pymc_experimental as pmx
+
+
+def test_match_gpytorch_linearcg_output():
+    N = 10
+    rank = 5
+    np.random.seed(1234)  # nans with seed 1234
+    K = np.random.randn(N, N)
+    K = K @ K.T + N * np.eye(N)
+    K_torch = torch.from_numpy(K)
+
+    L_gpt = gpytorch.pivoted_cholesky(K_torch, rank=rank, error_tol=1e-3)
+    L_np, _ = pmx.utils.pivoted_cholesky(K, max_iter=rank, error_tol=1e-3)
+    assert np.allclose(L_gpt, L_np.T)
