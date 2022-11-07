3eaa95a4a84ff1cdc5f37aa8861c0b9e37957050
HarrySpearing <37834138+HarrySpearing@users.noreply.github.com>
2022-11-07 09:57:53 +0000

    Mvncdf now working
    Can compute CDF, now work on gradients

--- pymc_experimental/distributions/mvncdf.py
+++ pymc_experimental/distributions/mvncdf.py
@@ -1,10 +1,12 @@
 import aesara
+import aesara.tensor as at
 from aesara.graph.basic import Apply
 from aesara.graph.op import Op
 from aesara.tensor.type import TensorType
 from aesara.tensor.random.basic import MvNormalRV
 from aeppl.logprob import _logcdf
 from scipy.stats import multivariate_normal
+
 # Op calculates mvncdf
 
 
@@ -33,55 +35,25 @@ def conditional_covariance(Sigma, mu, conditioned, conditioned_values):
 
 class Mvncdf(Op):
 
-  
-    itypes = [at.dvector] # expects a vector of parameter values when called
-    otypes = [at.dscalar] # outputs a single scalar value (the joint log cdf)
-
-    def __init__(self, multivariate_normal.logcdf, value, mu, cov):
-
-          """
-        The Op should calculate the log cdf of a multivariate normal when it is called.
-        
-        Parameters
-        ----------
-        logcdf:
-            the scipy logcdf function which calculates the log cdf of multivariate normal 
-            (https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.multivariate_normal.html)
-        value:
-            the value at which to evaluate the logcdf (x in scipy documentation)
-        mu: 
-            the mean vector of the distriution (mean in scipy docs)
-        cov:
-            the covariance matrix of the distribution
-        """
-        self.logcdf = scipy.stats.multivariate_normal.logcdf
-        self.value = value
-        self.mu = mu
-        self.cov = cov
-
     def make_node(self, value, mu, cov):
         value = at.as_tensor_variable(value)
         mu = at.as_tensor_variable(mu)
         cov = at.as_tensor_variable(cov)
 
-        return Apply(self, [value, mu, cov], [value.type()])
+        return Apply(self, [value, mu, cov], [value[0].type()])
 
     def perform(self, node, inputs, output_storage):
-
+        ## input and output should be np arrays
         value, mu, cov = inputs
-        output_storage[0] = self.logcdf(value, mu, cov)
+        output_storage[0][0] = multivariate_normal.logcdf(value, mu, cov)
 
-    def infer_shape(self, fgraph, node, i0_shapes):
-        return i0_shapes[0]  
+        
 
-    @_logcdf.register(MvNormalRV)
-    def mvnormal_logcdf(value, mu, cov):
-
-        return pm.logcdf(MvNormal.dist(mu, cov), value)
-
+    #def infer_shape(self, fgraph, node, i0_shapes):
+    #    return i0_shapes[0]
 
 mvncdf = Mvncdf()
 
 @_logcdf.register(MvNormalRV)
-def mvnormal_logcdf(value, mu, cov):
+def mvnormal_logcdf(op, value, rng, size, dtype, mu, cov, **kwargs):
     return mvncdf(value, mu, cov)
