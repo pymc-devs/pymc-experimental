d9574b2642b4e8aefdd7e8a074b1aefd27143527
Michal Raczycki <michalr265@gmail.com>
2023-04-04 15:09:33 +0100

    fixed naming not matching in __init__

--- pymc_experimental/version.py
+++ pymc_experimental/version.py
@@ -4,7 +4,7 @@ local_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def read_version():
-    version_file = os.path.join(here, "version.txt")
+    version_file = os.path.join(local_dir, "version.txt")
     with open(version_file, encoding="utf-8") as buff:
         return buff.read().splitlines()[0]
 
