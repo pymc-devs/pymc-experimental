ceecd15850e415d21f1e2bd0daebcae9f3912677
jessegrabowski <jessegrabowski@gmail.com>
2023-07-20 20:26:37 +0800

    Refactor shape checking to use `type.shape` Refactor `statespace.build_statespace_graph()` to use new distribution, preparing for simulation refactor

--- pymc_experimental/statespace/models/local_level.py
+++ pymc_experimental/statespace/models/local_level.py
@@ -12,12 +12,12 @@ _log = logging.getLogger("pymc.experimental.statespace")
 
 
 class BayesianLocalLevel(PyMCStateSpace):
-    def __init__(self, verbose=False):
+    def __init__(self, verbose=False, filter_type: str = "standard"):
         k_states = 2
         k_posdef = 2
         k_endog = 1
 
-        super().__init__(k_endog, k_states, k_posdef, verbose=verbose)
+        super().__init__(k_endog, k_states, k_posdef, filter_type=filter_type, verbose=verbose)
 
         # Initialize the matrices
         self.ssm["design"] = np.array([[1.0, 0.0]])
