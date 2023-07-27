1de05e87536743d8d309d6ada7b3ad99aa78dd2d
jessegrabowski <jessegrabowski@gmail.com>
2023-07-27 15:26:07 +0800

    Trying to get the API docs to render, revert local change to `pyproject.toml`

--- pymc_experimental/statespace/__init__.py
+++ pymc_experimental/statespace/__init__.py
@@ -1,5 +1,6 @@
+from pymc_experimental.statespace import core, filters, models
 from pymc_experimental.statespace.models.local_level import BayesianLocalLevel
 from pymc_experimental.statespace.models.SARIMAX import BayesianARIMA
 from pymc_experimental.statespace.models.VARMAX import BayesianVARMAX
 
-__all__ = ["BayesianLocalLevel", "BayesianARIMA", "BayesianVARMAX"]
+__all__ = ["BayesianLocalLevel", "BayesianARIMA", "BayesianVARMAX", "models", "core", "filters"]
