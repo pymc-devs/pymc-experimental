a2026f59e35cffdc4be3c1cfbb8bc25dff729be5
Bill Engels <w.j.engels@gmail.com>
2023-10-20 16:17:58 -0700

    fix import issue in dist math

--- pymc_experimental/tests/distributions/test_continuous.py
+++ pymc_experimental/tests/distributions/test_continuous.py
@@ -14,6 +14,7 @@
 import platform
 
 import numpy as np
+import numpy.testing as npt
 import pymc as pm
 
 # general imports
@@ -46,7 +47,7 @@ class TestPCPriorStudentT_dof:
     numerical approximation out to the tail.
     """
 
-    @pytest.mark.parameterize(
+    @pytest.mark.parametrize(
         "test_case",
         [
             {"U": 30, "alpha": 0.5, "dof": 5, "inla_result": -4.792407},
@@ -60,7 +61,7 @@ class TestPCPriorStudentT_dof:
     )
     def test_logp(self, test_case):
         d = PCPriorStudentT_dof.dist(U=test_case["U"], alpha=test_case["alpha"])
-        npt.assert_allclose(pm.logp(d, test_case["dof"]), test_case["inla_result"], rtol=0.1)
+        npt.assert_allclose(pm.logp(d, test_case["dof"]).eval(), test_case["inla_result"], rtol=0.1)
 
 
 class TestGenExtremeClass:
