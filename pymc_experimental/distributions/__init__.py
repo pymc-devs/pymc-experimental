b521e6410e96e66c43d2e85902cf1730161ddd47
Will Dean <57733339+wd60622@users.noreply.github.com>
2023-11-10 13:30:19 -0500

    Implement BetaNegativeBinomial distribution (#258)
    * Also broadcast parameters in Skellam when size is not provided
    
    Co-authored-by: Ricardo Vieira <28983449+ricardoV94@users.noreply.github.com>

--- pymc_experimental/distributions/__init__.py
+++ pymc_experimental/distributions/__init__.py
@@ -18,16 +18,22 @@ Experimental probability distributions for stochastic nodes in PyMC.
 """
 
 from pymc_experimental.distributions.continuous import Chi, GenExtreme
-from pymc_experimental.distributions.discrete import GeneralizedPoisson, Skellam
+from pymc_experimental.distributions.discrete import (
+    BetaNegativeBinomial,
+    GeneralizedPoisson,
+    Skellam,
+)
 from pymc_experimental.distributions.histogram_utils import histogram_approximation
 from pymc_experimental.distributions.multivariate import R2D2M2CP
 from pymc_experimental.distributions.timeseries import DiscreteMarkovChain
 
 __all__ = [
+    "BetaNegativeBinomial",
     "DiscreteMarkovChain",
     "GeneralizedPoisson",
     "GenExtreme",
     "R2D2M2CP",
+    "Skellam",
     "histogram_approximation",
     "Chi",
 ]
