fe33160b180eb774a2435b03d552eb875cbbf1fd
Carlos Iguaran <ciiguaran@gmail.com>
2023-11-15 16:58:02 -0300

    Removing extra comment

--- pymc_experimental/inference/smc/sampling.py
+++ pymc_experimental/inference/smc/sampling.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #   Copyright 2023 The PyMC Developers
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#!/usr/bin/env python
-# coding: utf-8
+
 import logging
 import time
 import warnings
