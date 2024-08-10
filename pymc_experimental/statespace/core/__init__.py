9f2d0e581956685195de5bc3d6195152298e1f06
jessegrabowski <jessegrabowski@gmail.com>
2024-08-10 14:10:50 +0800

    draft notebook

--- pymc_experimental/statespace/core/__init__.py
+++ pymc_experimental/statespace/core/__init__.py
@@ -1,5 +1,7 @@
-from pymc_experimental.statespace.core.compile import compile_statespace
+# ruff: noqa: I001
+
 from pymc_experimental.statespace.core.representation import PytensorRepresentation
 from pymc_experimental.statespace.core.statespace import PyMCStateSpace
+from pymc_experimental.statespace.core.compile import compile_statespace
 
 __all__ = ["PytensorRepresentation", "PyMCStateSpace", "compile_statespace"]
