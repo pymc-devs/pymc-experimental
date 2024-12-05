fa8dfffb6dac6280d6990033e15e14d0838e4d79
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2024-12-05 14:57:26 +0100

    Add Normal summary stats optimization

---
+++ pymc_experimental/sampling/optimizations/__init__.py
@@ -0,0 +1,13 @@
+# ruff: noqa: F401
+# Add rewrites to the optimization DBs
+import pymc_experimental.sampling.optimizations.summary_stats
+
+from pymc_experimental.sampling.optimizations.optimize import (
+    optimize_model_for_mcmc_sampling,
+    posterior_optimization_db,
+)
+
+__all__ = [
+    "posterior_optimization_db",
+    "optimize_model_for_mcmc_sampling",
+]
--- tests/sampling/mcmc/test_mcmc.py
+++ tests/sampling/mcmc/test_mcmc.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pytest
 
-from pymc.distributions import Beta, Binomial, InverseGamma
+from pymc.distributions import Beta, Binomial, HalfNormal, InverseGamma, Normal
 from pymc.model.core import Model
+from pymc.sampling.mcmc import sample
 from pymc.step_methods import Slice
 
 from pymc_experimental import opt_sample
@@ -18,3 +20,35 @@ def test_custom_step_raises():
             ValueError, match="The `step` argument is not supported in `opt_sample`"
         ):
             opt_sample(step=Slice([a, b]))
+
+
+def test_sample_opt_summary_stats(capsys):
+    rng = np.random.default_rng(3)
+    y_data = rng.normal(loc=1, scale=0.5, size=(1000,))
+
+    with Model() as m:
+        mu = Normal("mu")
+        sigma = HalfNormal("sigma")
+        y = Normal("y", mu=mu, sigma=sigma, observed=y_data)
+
+        sample_kwargs = dict(
+            chains=1, tune=500, draws=500, compute_convergence_checks=False, progressbar=False
+        )
+        idata = sample(**sample_kwargs)
+        # TODO: Make extract_data more robust to avoid this warning/error
+        #  Or alternatively extract data on the original model, not the optimized one
+        with pytest.warns(UserWarning, match="Could not extract data from symbolic observation"):
+            opt_idata = opt_sample(**sample_kwargs, verbose=True)
+
+    captured_out = capsys.readouterr().out
+    assert "Applied optimization: summary_stats_normal 1x" in captured_out
+
+    assert opt_idata.posterior.sizes["chain"] == 1
+    assert opt_idata.posterior.sizes["draw"] == 500
+    np.testing.assert_allclose(
+        idata.posterior["mu"].mean(), opt_idata.posterior["mu"].mean(), rtol=1e-2
+    )
+    np.testing.assert_allclose(
+        idata.posterior["sigma"].mean(), opt_idata.posterior["sigma"].mean(), rtol=1e-2
+    )
+    assert idata.sample_stats.sampling_time > opt_idata.sample_stats.sampling_time
